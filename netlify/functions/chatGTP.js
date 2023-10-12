4cc0d993bcfe7c48ab94618a84efaf34845265ba
marlonlynn <marlon@lucent.email>
2023-10-12 18:05:39 -0400

    test

---
+++ netlify/functions/chatGTP.js
@@ -0,0 +1,38 @@
+import fetch from "node-fetch";
+
+exports.handler = async function(event, context) {
+    if (event.httpMethod !== "POST") {
+        return { statusCode: 405, body: "Method Not Allowed" };
+    }
+
+    const { userInput } = JSON.parse(event.body);
+
+    const prompt = "Process the following home inspector's comment and provide a clear, concise explanation of the concern, showcasing the inspector's expertise. Ensure that the explanation includes why the subject in the comment is important to a home buyer, all in a way that an average 10th grader without construction knowledge can easily understand:\n\n[Inspector's Comment]: " + userInput;
+
+    const payload = {
+        "messages": [{"role": "system", "content": prompt}]
+    };
+
+    try {
+        const response = await fetch('https://api.openai.com/v1/engines/text-davinci-003/completions', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + process.env.YOUR_OPENAI_API_KEY // Make sure this key is set in Netlify's environment variables
+            },
+            body: JSON.stringify(payload)
+        });
+
+        const data = await response.json();
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: data.choices[0].message.content })
+        };
+    } catch (error) {
+        console.error("Error communicating with OpenAI:", error);
+        return {
+            statusCode: 500,
+            body: "Internal Server Error"
+        };
+    }
+};
--- script.js
+++ script.js
@@ -2,27 +2,28 @@ async function sendMessage() {
     const userInput = document.getElementById("userInput").value;
     const responseElement = document.getElementById("response");
 
-    // Construct the detailed prompt with the user's input embedded
-    const prompt = "Process the following home inspector's comment and provide a clear, concise explanation of the concern, showcasing the inspector's expertise. Ensure that the explanation includes why the subject in the comment is important to a home buyer, all in a way that an average 10th grader without construction knowledge can easily understand:\n\n[Inspector's Comment]: " + userInput;
-
     const payload = {
-        "messages": [{"role": "system", "content": prompt}]
+        userInput: userInput
     };
 
     try {
-        const response = await fetch('https://api.openai.com/v1/engines/text-davinci-003/completions', {
+        const response = await fetch("/.netlify/functions/chatGTP", {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer 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'
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify(payload)
         });
-        
+
         const data = await response.json();
-        responseElement.value = data.choices[0].message.content; // Set the response in the textarea
+        if(data && data.message) {
+            responseElement.value = data.message;
+        } else {
+            console.error("Unexpected response format:", JSON.stringify(data, null, 2));
+        }
+
     } catch (error) {
-        console.error("Error communicating with OpenAI:", error);
+        console.error("Error communicating with the function:", error);
     }
 }
 
